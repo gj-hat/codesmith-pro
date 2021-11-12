299b81b3463e29db2c5d2438131532ef40984a96
liuh <liuwei412552703@163.com>
2021-11-12 18:03:23 +0800

    [优化] 自定义定时任务处理功能.

---
+++ src/main/java/com/autogen/code/configuration/CustomizerTimeTask.java
@@ -0,0 +1,21 @@
+package com.autogen.code.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Ryan
+ * @email liuwei412552703@163.com
+ * @date 2021/11/12 18:01
+ */
+@Configuration
+public class CustomizerTimeTask {
+
+
+    // todo 2021年11月12日18:01:44  Ryan ： 定时任务
+
+
+
+
+
+
+}
