81faa80f8bebcabb3a28dc6c45b6ff82f99cb3fc
jiaguo <1520047927@qq.com>
2021-12-03 17:49:38 +0800

    [说明] 字符串处理工具类

--- src/main/java/com/autogen/code/utils/DataHandle.java
+++ src/main/java/com/autogen/code/utils/DataHandle.java
@@ -13,59 +13,87 @@ public class DataHandle {
 // TODO: 2021/12/3 待完善
 
 
+    public static void main(String[] args) {
+        System.out.println(slashToPort("User/account/name" ));
+    }
+
+
     /**
      * sql数据类型转java数据类型
-     * @param sqlTypeName     sql数据类型
-     * @return                java数据类型
+     *
+     * @param sqlTypeName sql数据类型
+     * @return java数据类型
      */
     public static String typeTransformation(String sqlTypeName) {
         return null;
     }
 
 
-
     /**
      * 首字母大写转小写
-     * @param st   大驼峰字符串
-     * @return     小驼峰字符串
+     *
+     * @param st 大驼峰字符串
+     * @return 小驼峰字符串
      */
-    public static String toLow(String st){
-        return null;
+    public static String bigToLow(String st) {
+        char[] stArr = st.toCharArray();
+        if ((stArr[0] >= 65) && (stArr[0] <= 90)) {
+            stArr[0] += 32;
+        }
+        return String.valueOf(stArr);
     }
-
 
 
     /**
      * 首字母小写转大写
-     * @param st   小驼峰字符串
-     * @return     大驼峰字符串
+     *
+     * @param st 小驼峰字符串
+     * @return 大驼峰字符串
      */
-    public static String toBig(String st){
-        return null;
+    public static String lowToBig(String st) {
+        char[] stArr = st.toCharArray();
+        if ((stArr[0] >= 97) && (stArr[0] <= 122)) {
+            stArr[0] -= 32;
+        }
+        return String.valueOf(stArr);
     }
-
-
 
 
     /**
      * sql经典命名转java经典命名
-     * @param string    下划线命名字符串
-     * @return           大驼峰字符串
+     *
+     * @param st 下划线命名字符串
+     * @return 驼峰字符串   不做大小写处理
      */
-    public static String toBigHump(String string) {
-        return null;
+    public static String underlineToHump(String st) {
+        return st.replaceAll("_", "/" );
+
     }
+
+
 
 
     /**
-     * sql经典命名转java经典命名小驼峰
-     * @param string      下划线命名字符串
-     * @return            大驼峰字符串
+     * 将传入的字符串按顺序把"点" --- "斜线"
+     *
+     * @param st 不定长参数
+     * @return   string
      */
-    public static String toSmallHump(String string) {
-        return null;
+    public static String pointToSlash(String st) {
+        return st.replaceAll("\\.", "/" );
     }
 
+    /**
+     * 将传入的字符串按顺序把"反斜线"  ---  "点"
+     *
+     * @param st 不定长参数
+     * @return    string
+     */
+    public static String slashToPort(String st) {
+
+
+        return st.replaceAll("/", "." );
+    }
 
 
 }
